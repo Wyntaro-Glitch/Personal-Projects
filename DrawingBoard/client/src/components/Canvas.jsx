408cc6c7e399ecc89cfd784fb8d054817b4377f2
Wyntaro-Glitch <sherwincalantoc.developer@gmail.com>
2026-09-02 03:04:32 +0800

    Center canvas with flexbox instead of absolute positioning

--- DrawingBoard/client/src/components/Canvas.jsx
+++ DrawingBoard/client/src/components/Canvas.jsx
@@ -372,10 +372,7 @@ export default function Canvas({
       ref={containerRef}
       className="canvas-container"
       style={{ 
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
+        position: 'relative',
         overflow: 'visible'
       }}
     >
