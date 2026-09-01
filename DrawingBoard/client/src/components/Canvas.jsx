eeedb4e7aa859db36c9902816e59824b57c12d1c
Wyntaro-Glitch <sherwincalantoc.developer@gmail.com>
2026-09-02 02:57:11 +0800

    Center canvas vertically and horizontally in canvas-area

--- DrawingBoard/client/src/components/Canvas.jsx
+++ DrawingBoard/client/src/components/Canvas.jsx
@@ -373,8 +373,7 @@ export default function Canvas({
       className="canvas-container"
       style={{ 
         position: 'relative',
-        width: '100%',
-        height: '100%',
+        flexShrink: 0,
         overflow: 'hidden'
       }}
     >
